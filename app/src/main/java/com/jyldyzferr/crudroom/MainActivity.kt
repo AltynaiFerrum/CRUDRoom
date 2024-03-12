fa0fa0bcba854e88c5519def52d782853899603f
Altynai Ferrum <pr.galaxy812@gmail.com>
2024-03-12 13:14:24 +0600

    Initial commit

---
+++ app/src/main/java/com/jyldyzferr/crudroom/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jyldyzferr.crudroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.jyldyzferr.crudroom.database.NoteDatabase
+import com.jyldyzferr.crudroom.repository.NoteRepository
+import com.jyldyzferr.crudroom.viewmodel.NoteViewModel
+import com.jyldyzferr.crudroom.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
