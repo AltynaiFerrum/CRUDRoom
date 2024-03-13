3604fc4525a4194fcbef7430d696e0be83aa239c
Altynai Ferrum <pr.galaxy812@gmail.com>
2024-03-13 11:46:18 +0600

    Second commit

--- app/src/main/java/com/jyldyzferr/crudroom/fragments/HomeFragment.kt
+++ app/src/main/java/com/jyldyzferr/crudroom/fragments/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
@@ -44,6 +44,7 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         notesViewModel = (activity as MainActivity).noteViewModel
+        setupHomeRecyclerView()
 
         binding.addNoteFab.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_addNoteFragment)
@@ -53,11 +54,11 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
     private fun updateUI(note: List<Note>?) {
         if (note != null) {
             if (note.isEmpty()) {
-                binding.emptyNotesImage.visibility = View.GONE
-                binding.homeRecyclerView.visibility = View.VISIBLE
-            } else {
                 binding.emptyNotesImage.visibility = View.VISIBLE
                 binding.homeRecyclerView.visibility = View.GONE
+            } else {
+                binding.emptyNotesImage.visibility = View.GONE
+                binding.homeRecyclerView.visibility = View.VISIBLE
             }
         }
     }
@@ -77,22 +78,40 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
         }
     }
 
+    private fun searchNote(query: String?) {
+        val searchQuery = "%$query"
+
+        notesViewModel.searchNote(searchQuery).observe(this) { list ->
+            noteAdapter.differ.submitList(list)
+        }
+    }
+
     override fun onQueryTextSubmit(query: String?): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 
     override fun onQueryTextChange(newText: String?): Boolean {
-        TODO("Not yet implemented")
+        if (newText != null) {
+            searchNote(newText)
+        }
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        homeBinding = null
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menu.clear()
         menuInflater.inflate(R.menu.home_menu, menu)
 
-        val menuSearch = menu.findItem(R.id.searchMenu)
+        val menuSearch = menu.findItem(R.id.searchMenu).actionView as SearchView
+        menuSearch.isSubmitButtonEnabled = false
+        menuSearch.setOnQueryTextListener(this)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 }
